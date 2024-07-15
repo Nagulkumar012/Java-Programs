a3faef2a510c847c3754e7d1070d6a88be58dca0
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:03:53 +0530

    Update and rename friendlypair.java to Friendly_pair.java

--- Friendly_pair.java
+++ Friendly_pair.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class friendlypair {
+public class Friendly_pair {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
