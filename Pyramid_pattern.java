029d43c0ce18770544d36bbb96199a97a3691cd5
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:05:37 +0530

    Update and rename pyramidpattern.java to Pyramid_pattern.java

--- Pyramid_pattern.java
+++ Pyramid_pattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class pyramidpattern {
+public class Pyramid_pattern {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
