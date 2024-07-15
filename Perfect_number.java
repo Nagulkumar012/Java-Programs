a022473c01240052111ab30cc208754369ad2100
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:05:17 +0530

    Update and rename perfect.java to Perfect_number.java

--- Perfect_number.java
+++ Perfect_number.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class perfect {
+public class Perfect_number {
  public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
