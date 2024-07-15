d5b963860352359a8b2ba55b121c3a8f3f6685fd
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:08:02 +0530

    Update and rename two_dimensional_identical.java to Two_Dimensional_Identical.java

--- Two_Dimensional_Identical.java
+++ Two_Dimensional_Identical.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class two_dimensional_identical {
+public class Two_Dimensional_Identical {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int row=sc.nextInt();
