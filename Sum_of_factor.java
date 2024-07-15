a1f6ad92e788098a581ae1b1cfc64854f0d04231
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:07:35 +0530

    Update and rename sumoffactor.java to Sum_of_factor.java

--- Sum_of_factor.java
+++ Sum_of_factor.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class sumoffactor {
+public class Sum_of_factor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a=sc.nextInt();
