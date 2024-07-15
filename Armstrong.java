b762bf435ca92d186351863b25fb9418504ea574
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:01:45 +0530

    Update and rename armstrong.java to Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class armstrong {
+public class Armstrong {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
