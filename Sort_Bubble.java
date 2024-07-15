c7b8295ff2ed8c652b59b7f703f2139eab3624ab
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:07:05 +0530

    Update and rename sortbubble.java to Sort_Bubble.java

--- Sort_Bubble.java
+++ Sort_Bubble.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class sortbubble {
+public class Sort_Bubble {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
