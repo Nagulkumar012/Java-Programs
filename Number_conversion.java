4ebb4cd0357503bb1185d257b1e7ecd829ea0ca7
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:04:54 +0530

    Update and rename numberconversion.java to Number_conversion.java

--- Number_conversion.java
+++ Number_conversion.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class numberconversion {
+public class Number_conversion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
