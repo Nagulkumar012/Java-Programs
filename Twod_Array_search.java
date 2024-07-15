4e00785b337aabf1b1d4ce0f369399a9b1c51ff4
Nagulkumar012 <nagulp2003@gmail.com>
2024-07-15 11:08:49 +0530

    Update and rename twodarraysearch.java to Twod_Array_search.java

--- Twod_Array_search.java
+++ Twod_Array_search.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class twodarraysearch {
+public class Twod_Array_search {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int i, j;
